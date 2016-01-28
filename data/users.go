03111903eb22d2948a258a7b883a9818e7e1044f
Jason Connell <jasontconnell@gmail.com>
2016-01-28 00:17:04 -0500

    Updating types for server info

--- data/users.go
+++ data/users.go
@@ -6,6 +6,8 @@ import (
 
 type User struct {
 	Id bson.ObjectId `bson:"_id,omitempty" json:"_id"`
+	FirstName string `bson:"firstName" json:"firstName"`
+	LastName string `bson:"lastName" json:"lastName"`
 	Email string `bson:"email" json:"email"`
 	Username string `bson:"username" json:"username"`
 	Password string `bson:"password" json:"password"`
